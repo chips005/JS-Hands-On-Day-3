3b14bc41dd005f7c98391533dd2340acbf241d9e
Zack P <chips005@gmail.com>
2017-06-20 22:14:35 -0700

    4th commit

---
+++ script3.js
@@ -0,0 +1,39 @@
+class Person {
+  constructor (hair color, eye color, vertical){
+    this.hair color = hair color;
+    this.eye color = eye color;
+    this.vertical = vertical;
+  }
+}
+
+class grandParents {
+  constructor(hair color, eye color, vertical){
+    super(grey, brown, 23 inches);
+  }
+}
+
+class Parents {
+  constructor (hair color, eye color, vertical){
+    super (brown, brown, 19 inches);
+  }
+}
+
+class Children {
+  constructor (hair color, eye color, vertical){
+    super (brown, blue, 20 inches);
+  }
+}
+
+class grandChildren {
+  constructor (hair color, eye color, vertical){
+    super (red, green, 16 inches);
+}
+
+
+
+let famArray = ['grandParents', 'Parents', 'Children', 'grandChildren'];
+
+for (var i = 0, i < famArray.length, i++)
+  function vertical = {
+
+  }
